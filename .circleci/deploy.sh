c13cd5e08a81b638dac46b3fcbc8c12411f443ea
G3B3 <gentilboulet@gmail.com>
2019-08-10 00:04:35 +0200

    Circleci/autodeploy (#48)
    * CircleCI automatic deployment
    * Fixing deploy key
    * Update deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,25 +1,29 @@
-!/bin/#!/usr/bin/env bash
+#!/usr/bin/env bash
 
- if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
-    git config --global user.email $GH_EMAIL
-    git config --global user.name $GH_NAME
+set -e
 
-    git clone $CIRCLE_REPOSITORY_URL gh-pages
+if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
+  mkdir -p ~/.ssh
+  ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-    cd gh-pages
-    git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-    git rm -rf .
-    cd ..
+  yes | git clone $CIRCLE_REPOSITORY_URL gh-pages
 
-    npm run build
+  pushd gh-pages
 
-    cp -a build/. gh-pages/.
+  git config user.name "CircleCI auto deploy"
+  git config user.email "circleci@lotwcs.com"
 
-    mkdir -p gh-pages/.circleci && cp -a .circleci/. gh-pages/.circleci/.
-    cd gh-pages
+  git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+  git rm -rf .
+  popd ..
 
-    git add -A
-    git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+  cp -a build/. gh-pages/.
 
-    git push origin $TARGET_BRANCH
-  fi
+  mkdir -p gh-pages/.circleci && cp -a .circleci/. gh-pages/.circleci/.
+  pushd gh-pages
+
+  git add -A
+  git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+
+  git push origin $TARGET_BRANCH
+fi
