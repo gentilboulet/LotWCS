73810e7f05c18a6d126133afd2d7b6362f22e963
CircleCI auto deploy <circleci@lotwcs.com>
2019-08-09 22:20:41 +0000

    Automated deployment to GitHub Pages: cb2aea32f440b78adaf9471b13e7229a17eaa566

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
- if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
-    mkdir -p ~/.ssh
-    ssh-keyscan github.com >> ~/.ssh/known_hosts
+if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
+  mkdir -p ~/.ssh
+  ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-    git clone $CIRCLE_REPOSITORY_URL gh-pages
+  yes | git clone $CIRCLE_REPOSITORY_URL gh-pages
 
-    cd gh-pages
+  pushd gh-pages
 
-    git config user.name "CircleCI auto deploy"
-    git config user.email "circleci@lotwcs.com"
+  git config user.name "CircleCI auto deploy"
+  git config user.email "circleci@lotwcs.com"
 
-    git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-    git rm -rf .
-    cd ..
+  git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+  git rm -rf .
+  popd
 
-    cp -a build/. gh-pages/.
+  cp -a build/. gh-pages/.
 
-    mkdir -p gh-pages/.circleci && cp -a .circleci/. gh-pages/.circleci/.
-    cd gh-pages
+  mkdir -p gh-pages/.circleci && cp -a .circleci/. gh-pages/.circleci/.
+  pushd gh-pages
 
-    git add -A
-    git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+  git add -A
+  git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
 
-    git push origin $TARGET_BRANCH
-  fi
+  git push origin $TARGET_BRANCH
+fi
---
+++ precache-manifest.d084107947204e108782492812463f75.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788adb2ab43257004bd5c5e64e2ba2be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c4744a06fcbc97979bb",
+    "url": "/static/css/2.7bbd6671.chunk.css"
+  },
+  {
+    "revision": "e28e82b7051d70ac3331",
+    "url": "/static/css/main.e91e8ef8.chunk.css"
+  },
+  {
+    "revision": "0c4744a06fcbc97979bb",
+    "url": "/static/js/2.1c68a691.chunk.js"
+  },
+  {
+    "revision": "e28e82b7051d70ac3331",
+    "url": "/static/js/main.2326b995.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
