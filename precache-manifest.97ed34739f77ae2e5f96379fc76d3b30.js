a66dd7fd977cde41726b2fd0b1f75e2697fd842b
CircleCI auto deploy <circleci@lotwcs.com>
2019-08-09 22:51:07 +0000

    Automated deployment to GitHub Pages: 05335f19a880293c6294bcc5d901254e111d204f

---
+++ precache-manifest.97ed34739f77ae2e5f96379fc76d3b30.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cada6fd3c45c4f2dcedea86f30d77676",
+    "url": "/LotWCS/index.html"
+  },
+  {
+    "revision": "1d642fd718c5d1fc98c5",
+    "url": "/LotWCS/static/css/2.0da0d610.chunk.css"
+  },
+  {
+    "revision": "571d4de6207f46bea0a7",
+    "url": "/LotWCS/static/css/main.e91e8ef8.chunk.css"
+  },
+  {
+    "revision": "1d642fd718c5d1fc98c5",
+    "url": "/LotWCS/static/js/2.1c68a691.chunk.js"
+  },
+  {
+    "revision": "571d4de6207f46bea0a7",
+    "url": "/LotWCS/static/js/main.e473dfe0.chunk.js"
+  },
+  {
+    "revision": "5a029ddd8c27d9ead8d8",
+    "url": "/LotWCS/static/js/runtime~main.61174c03.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/LotWCS/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/LotWCS/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/LotWCS/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/LotWCS/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/LotWCS/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d084107947204e108782492812463f75.js"
+  "/LotWCS/precache-manifest.97ed34739f77ae2e5f96379fc76d3b30.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LotWCS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
