2a9bf2c2cc1c2b109d8f7b651c0d9120a9cb1183
G3B3 <gentilboulet@gmail.com>
2020-01-22 22:14:35 +0100

    Updated corejs && typescript

--- src/state/character/actions/chi.test.tsx
+++ src/state/character/actions/chi.test.tsx
@@ -6,7 +6,6 @@ describe("Testing chi action creator", () => {
     const chiType = "fire";
     const value = 123;
     const action = actions.chiBuy(chiType, value, zeroCost);
-    console.log(action);
     expect(action.payload.chi).toBe(chiType);
     expect(action.payload.value).toBe(value);
     expect(action).toMatchSnapshot();
